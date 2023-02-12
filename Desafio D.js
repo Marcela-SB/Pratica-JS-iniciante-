6c616bb41e163cc7a0e086b388e8bc3bb0a176a0
Marcela Silva <110864304+Marci-Hime@users.noreply.github.com>
2023-02-12 16:14:47 -0300

    Update and rename Questão 2 D.html to Desafio D.js

---
+++ Desafio D.js
@@ -0,0 +1,17 @@
+// D) ENTRE COM 6 NÚMEROS E IMPRIMA O QUADRADO DE CADA NÚMERO.
+
+var n1, n2, n3, n4, n5, n6;
+
+n1=parseFloat(prompt("Escreva o primeiro número..."));
+n2=parseFloat(prompt("Escreva o segundo número..."));
+n3=parseFloat(prompt("Escreva o terceiro número..."));
+n4=parseFloat(prompt("Escreva o quarto número..."));
+n5=parseFloat(prompt("Escreva o quinto número..."));
+n6=parseFloat(prompt("Escreva o sexto número..."));
+
+document.write("<p>Primeiro número = "+n1+" ---------- "+"Quadrado = "+(n1*n1)+"</p>");
+document.write("<p>Segundo número = "+n2+" ---------- "+"Quadrado = "+(n2*n2)+"</p>");
+document.write("<p>Terceiro número = "+n3+" ---------- "+"Quadrado = "+(n3*n3)+"</p>");
+document.write("<p>Quarto número = "+n4+" ---------- "+"Quadrado = "+(n4*n4)+"</p>");
+document.write("<p>Quinto número = "+n5+" ---------- "+"Quadrado = "+(n5*n5)+"</p>");
+document.write("<p>Sexto número = "+n6+" ---------- "+"Quadrado = "+(n6*n6)+"</p>");
